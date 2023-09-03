fcf9351c81672581ac2235ea399d35e2a6c4c5f0
Monu0777 <143521089+Monu0777@users.noreply.github.com>
2023-09-03 16:58:22 +0530

    Add files via upload

--- Bikash/core/userbot.py
+++ Bikash/core/userbot.py
@@ -48,8 +48,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("BikashGadgetsTech")
-                await self.one.join_chat("BikashGadgetsTech")
+                await self.one.join_chat("About_Info_Devil")
+                await self.one.join_chat("About_Info_Devil")
             except:
                 pass
             assistants.append(1)
@@ -110,8 +110,8 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("BikashGadgetsTech")
-                await self.three.join_chat("BikashGadgetsTech")
+                await self.three.join_chat("About_Info_Devil")
+                await self.three.join_chat("About_Info_Devil")
             except:
                 pass
             assistants.append(3)
@@ -141,8 +141,8 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("BikashGadgetsTech")
-                await self.four.join_chat("BikashGadgetsTech")
+                await self.four.join_chat("About_Info_Devil")
+                await self.four.join_chat("About_Info_Devil")
             except:
                 pass
             assistants.append(4)
@@ -172,8 +172,8 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("BikashGadgetsTech")
-                await self.five.join_chat("BikashGadgetsTech")
+                await self.five.join_chat("About_Info_Devil")
+                await self.five.join_chat("About_Info_Devil")
             except:
                 pass
             assistants.append(5)
