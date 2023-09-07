936ff4a101ae4cd9cae83d86cc2569a52edefba6
Monu0777 <143521089+Monu0777@users.noreply.github.com>
2023-09-07 19:02:30 +0530

    Add files via upload

---
+++ plugins/extra/Owner2.0.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+
+from Bikash import app
+from Bikash.utils.bgtmusic.bk import command
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+
+
+@app.on_message(
+    filters.command("owner 2.0")
+    & filters.group
+    & ~filters.edited & filters.group & ~filters.edited)
+async def bikash(client: Client, message: Message):
+    await message.reply_photo(
+        photo=f"https://telegra.ph/file/1e5e9c8021f5932cc3b57.jpg",
+        caption=f"""🥀 𝐃𝐞𝐯𝐢𝐥 𝐈𝐬 𝐎𝐰𝐧𝐞𝐫 𝐎𝐟 𝐁𝐠𝐭 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 🌺, 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧 𝐅𝐨𝐫 𝐂𝐨𝐧𝐭𝐚𝐜𝐭 𝐃𝐞𝐯𝐢𝐥 ♕, 𝐈𝐟 𝐘𝐨𝐮 𝐖𝐚𝐧𝐭 𝐏𝐫𝐨𝐦𝐨𝐭𝐞 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬 𝐎𝐫 𝐎𝐭𝐡𝐞𝐫𝐬 𝐋𝐢𝐧𝐤, 𝐓𝐡𝐞𝐧 𝐂𝐥𝐢𝐜𝐤 𝐏𝐫𝐨𝐦𝐨𝐭𝐢𝐨𝐧 𝐁𝐮𝐭𝐭𝐨𝐧 𝐂𝐥𝐢𝐜𝐤 𝐎𝐭𝐡𝐞𝐫𝐬 𝐁𝐮𝐭𝐭𝐨𝐧 & 𝐉𝐨𝐢𝐧 𝐎𝐮𝐫 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 𝐎𝐫 𝐆𝐫𝐨𝐮𝐩.. 🥀 [𝐘𝐨𝐮𝐭𝐮𝐛𝐞](https://youtube.com/@motivationalforall9823?si=YZNPKGhePON-2j9k)""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "🥀 𝐃𝐞𝐯𝐢𝐥 𝐇𝐞𝐚𝐯𝐞𝐧 🥀", url=f"https://t.me/Monu_Gupta_01")
+            ],          
+            [
+                    InlineKeyboardButton(
+                        "🥀 𝐏𝐫𝐨𝐦𝐨𝐭𝐢𝐨𝐧 🥀", url=f"https://t.me/Monu_Gupta_01")
+                ],
+                [
+                    InlineKeyboardButton(
+                        "🥀 𝐒𝐮𝐩𝐩𝐨𝐫𝐭 🥀", url=f"https://t.me/About_Info_Devil"
+                    ),
+                    InlineKeyboardButton(
+                        "🥀 𝐔𝐩𝐝𝐚𝐭𝐞𝐬 🥀", url=f"https://t.me/About_Info_Devil")
+                ]
+            ]
+        ),
+    )
