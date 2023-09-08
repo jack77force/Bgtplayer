49571082c1b64be56a8c7bf368381fdb0f58175a
Monu0777 <143521089+Monu0777@users.noreply.github.com>
2023-09-08 23:49:13 +0530

    Add files via upload

--- plugins/extra/shayri.py
+++ plugins/extra/shayri.py
@@ -5,43 +5,31 @@ import os
 import re
 import asyncio
 import time
+
 from Bikash import app
 from Bikash.utils.bgtmusic.bk import command
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-shayri = [ " 🌺बहुत अच्छा लगता है तुझे सताना और फिर प्यार से तुझे मनाना।🌺 \n\n🥀Bahut aacha lagta hai tujhe satana Aur fir pyar se tujhe manana.🥀 ",
-           " 🌺मेरी जिंदगी मेरी जान हो तुम मेरे सुकून का दुसरा नाम हो तुम।🌺 \n\n🥀Meri zindagi Meri jaan ho tum Mere sukoon ka Dusra naam ho tum.🥀 " ]
 
 @app.on_message(
-    filters.command(shayri)
+    filters.command("shayri")
     & filters.group
     & ~filters.edited & filters.group & ~filters.edited)
-async def help(client: Client, message: Message):
-    await message.reply_text(
-        text = random.choice(shayri),
+async def bikash(client: Client, message: Message):
+    await message.reply_text(text = random.choice(shayri),
+        photo=f"https://telegra.ph/file/47965cd5bedfb980059a6.jpg",
+        caption=f"""🥀 𝐃𝐞𝐯𝐢𝐥 𝐈𝐬 𝐎𝐰𝐧𝐞𝐫 𝐎𝐟 𝐃𝐞𝐯𝐢𝐥 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 🌺, 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧 𝐅𝐨𝐫 𝐋𝐢𝐯𝐞 𝐐𝐮𝐢𝐳 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 & 𝐉𝐨𝐢𝐧 𝐍𝐨𝐰 [𝐃𝐞𝐯𝐢𝐥](https://t.me/Monu_Gupta_01)""",
+        caption=f"""Hello Guys"""
         reply_markup=InlineKeyboardMarkup(
             [
+                
                 [
                     InlineKeyboardButton(
-                        "✨𝐒υρρσɾ𝐓✨", url=f"https://t.me/TKS_JOIN"),
-                     ]
-            ]
-        ),
-    )
-
-@app.on_message(
-    filters.command(shayri)
-    & filters.private
-    & ~filters.edited & filters.private & ~filters.edited)
-async def help(client: Client, message: Message):
-    await message.reply_text(
-        text = random.choice(shayri),
-        reply_markup=InlineKeyboardMarkup(
-            [
-                [
+                        "🥀 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 🥀", url=f"https://t.me/mission_successs"
+                    ),
                     InlineKeyboardButton(
-                        "💘𝐂ԋαɳɳҽ𝐋💘", url=f"https://t.me/TKS_JOIN"),
-                     ]
+                        "🥀 𝐉𝐨𝐢𝐧 𝐍𝐨𝐰 🥀", url=f"https://t.me/Current_Affairs_Zone_2")
+                ]
             ]
         ),
     )
