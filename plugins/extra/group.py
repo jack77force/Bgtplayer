99c1db9716a1b7ea13b1c0d7508774d0443b8f4b
Monu0777 <143521089+Monu0777@users.noreply.github.com>
2023-09-08 22:23:17 +0530

    Add files via upload

--- plugins/extra/group.py
+++ plugins/extra/group.py
@@ -11,7 +11,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
     & ~filters.edited & filters.group & ~filters.edited)
 async def bikash(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/1e5e9c8021f5932cc3b57.jpg",
+        photo=f"https://telegra.ph/file/47965cd5bedfb980059a6.jpg",
         caption=f"""🥀 𝐃𝐞𝐯𝐢𝐥 𝐈𝐬 𝐎𝐰𝐧𝐞𝐫 𝐎𝐟 𝐃𝐞𝐯𝐢𝐥 𝐌𝐮𝐬𝐢𝐜 𝐁𝐨𝐭 🌺, 𝐂𝐥𝐢𝐜𝐤 𝐁𝐞𝐥𝐨𝐰 𝐁𝐮𝐭𝐭𝐨𝐧 𝐅𝐨𝐫 𝐋𝐢𝐯𝐞 𝐐𝐮𝐢𝐳 𝐂𝐥𝐢𝐜𝐤 𝐇𝐞𝐫𝐞 & 𝐉𝐨𝐢𝐧 𝐍𝐨𝐰 [𝐃𝐞𝐯𝐢𝐥](https://t.me/Monu_Gupta_01)""",
         reply_markup=InlineKeyboardMarkup(
             [
